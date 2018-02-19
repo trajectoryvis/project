4e315f4735282312f904613f240e0a7964232fda
joe <joekelof.19.94@gmail.com>
2018-02-19 15:00:49 +0100

    en bit på vägen

--- JSONcreator/src/Main.java
+++ JSONcreator/src/Main.java
@@ -1,4 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+	private static String inputpath = "C:/Users/Joe/Documents/Skola/kex/project/points_no_text.txt";
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		InputStream in = new FileInputStream(inputpath);
+		Reader reader = new Reader(in);
+		ArrayList<String> objects = reader.read();
+	}//main
 
-}
+}//class
--- JSONcreator/src/Reader.java
+++ JSONcreator/src/Reader.java
@@ -1,4 +1,30 @@
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Reader {
-
-}
+	private InputStream in;
+	private int personnr = 0;
+	
+	public Reader(InputStream in){
+		this.in = in;
+	}//constructor
+	
+	public ArrayList<String> read(){
+		ArrayList<String> objects = new ArrayList<String>();
+		Scanner scanner = new Scanner(in);
+		scanner.useDelimiter("\n");
+		System.out.println("Number of splines/people: " + scanner.next()); //the first line, number of splines/persons
+		
+		while(scanner.hasNext()){
+			int controlpoints = Integer.parseInt(scanner.next().replaceAll("\\p{C}", ""));	//apparently there are some unprintable hidden characters in the text causing errors, replace them with nothing
+			for(int i = 0; i < controlpoints; i++){
+				scanner.next();
+			}
+			personnr++;
+		}
+		return objects;
+	}
+	
+}//class
